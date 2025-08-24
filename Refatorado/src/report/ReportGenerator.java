e4101017cf270d1d639f23c173c8c17ac40f368d
Flavio-Costa-Moreira <20231tadssaj0001@ifba.edu.br>
2025-08-23 22:46:52 -0300

    report

---
+++ Refatorado/src/report/ReportGenerator.java
@@ -0,0 +1,20 @@
+package com.smartroom.report;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReportGenerator {
+    public Path toCsv(List<SensorData> data, Path out) throws IOException {
+        if (out.getParent() != null) Files.createDirectories(out.getParent());
+        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(out))) {
+            pw.println("timestamp,sensorId,type,value");
+            for (SensorData d : data) {
+                pw.printf("%s,%s,%s,%.2f%n", d.getTimestamp(), d.getSensorId(), d.getType(), d.getValue());
+            }
+        }
+        return out;
+    }
+}
---
+++ Refatorado/src/report/SensorData.java
@@ -0,0 +1,50 @@
+package com.smartroom.report;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class SensorData {
+    private final String sensorId;
+    private final String type;
+    private final double value;
+    private final Instant timestamp;
+
+    public SensorData(String sensorId, String type, double value, Instant timestamp) {
+        this.sensorId = sensorId;
+        this.type = type;
+        this.value = value;
+        this.timestamp = timestamp == null ? Instant.now() : timestamp;
+    }
+
+    public String getSensorId() { return sensorId; }
+    public String getType() { return type; }
+    public double getValue() { return value; }
+    public Instant getTimestamp() { return timestamp; }
+
+    @Override
+    public String toString() {
+        return "SensorData{" +
+                "sensorId='" + sensorId + ''' +
+                ", type='" + type + ''' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SensorData)) return false;
+        SensorData that = (SensorData) o;
+        return Double.compare(that.value, value) == 0 &&
+                Objects.equals(sensorId, that.sensorId) &&
+                Objects.equals(type, that.type) &&
+                Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sensorId, type, value, timestamp);
+    }
+}
+
